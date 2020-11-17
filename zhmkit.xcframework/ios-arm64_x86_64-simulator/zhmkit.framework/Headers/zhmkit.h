9109c870da7151b8046ffbf5816c43b037cee239
Homer Zuo <homerzhm@gmail.com>
2020-11-16 22:05:45 -0500

    initial of 0.0.2

---
+++ zhmkit.xcframework/ios-arm64_x86_64-simulator/zhmkit.framework/Headers/zhmkit.h
@@ -0,0 +1,18 @@
+//
+//  zhmkit.h
+//  zhmkit
+//
+//  Created by Hongming Zuo on 2020-11-16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for zhmkit.
+FOUNDATION_EXPORT double zhmkitVersionNumber;
+
+//! Project version string for zhmkit.
+FOUNDATION_EXPORT const unsigned char zhmkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <zhmkit/PublicHeader.h>
+
+
